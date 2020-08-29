ed04f0fc22e1a175b44161d3461644584f46dddb
1139556759@qq.com <1139556759@qq.com>
2020-08-29 14:53:47 +0800

    publish upload

--- src/pages/upload/index.vue
+++ src/pages/upload/index.vue
@@ -1,10 +1,53 @@
 <template>
-  <div>upload</div>
+  <div class="container">
+    <div style="margin: 20px 0">
+      <div style="margin: 10px 0">普通上传</div>
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove"
+      >
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+    </div>
+    <span>拖拽上传</span>
+    <el-upload
+      class="upload-demo"
+      drag
+      action="https://jsonplaceholder.typicode.com/posts/"
+      multiple
+      style="margin: 20px 0"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">
+        将文件拖到此处，或
+        <em>点击上传</em>
+      </div>
+      <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+    </el-upload>
+  </div>
 </template>
 
 <script>
 export default {
   name: "upload",
+  data() {
+    return {
+      dialogImageUrl: "",
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+  },
 };
 </script>
 
